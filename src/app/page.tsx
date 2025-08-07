7193aec4cce9fc18443073fbfac92ef060991902
palakmishra32 <palakmishra3284@gmail.com>
2025-08-07 22:09:59 +0530

    updated page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-   <div className="text-4xl font-bold text-green-500">
-     Hello
-     </div>
-  )
-}
+   <Button>
+    click me
+   </Button>
+  );
+};
